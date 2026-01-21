d10b774aee288fb3e5906f77972349e3c99533cd
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2026-01-21 23:42:17 +0200

    Update Movies.jsx

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,5 +1,35 @@
+import { useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { fetchMovieByQuery } from 'services/api';
+import MovieList from 'components/MovieList/MovieList';
+
 export default function Movies() {
-    return (
-        <h1>Movies pages</h1>
-    )
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query') ?? '';
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    if (!query) return;
+
+    fetchMovieByQuery(query)
+      .then(data => setMovies(data.results))
+      .catch(console.error);
+  }, [query]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const value = e.target.elements.query.value.trim();
+    if (!value) return;
+    setSearchParams({ query: value });
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input name="query" />
+        <button type="submit">Search</button>
+      </form>
+
+      <MovieList movies={movies} />
+    </>
+  );
 }
